f3a56c243d9d3afe6db4dd5b8cff94d2ef8f2998
Lucy <lucynasztemberg@o2.pl>
2018-09-11 14:16:41 +0200

    Add getters and setters

--- src/main/java/pl/xcodesoftware/rekrutacja/Entities/NumbersToSort.java
+++ src/main/java/pl/xcodesoftware/rekrutacja/Entities/NumbersToSort.java
@@ -15,6 +15,22 @@ public class NumbersToSort {
     @JsonProperty("order")
     public String order;
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
     public NumbersToSort() {
     }
 }
