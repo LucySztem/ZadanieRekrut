d9def6d9574acd7a18197cd7620416f69d4acef1
Lucy <lucynasztemberg@o2.pl>
2018-09-11 21:21:18 +0200

    Correct import due to package change

--- src/main/java/pl/xcodesoftware/rekrutacja/controllers/EndpointOneController.java
+++ src/main/java/pl/xcodesoftware/rekrutacja/controllers/EndpointOneController.java
@@ -1,4 +1,4 @@
-package pl.xcodesoftware.rekrutacja.Controllers;
+package pl.xcodesoftware.rekrutacja.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
