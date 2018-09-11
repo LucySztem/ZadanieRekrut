e6b28b7e8709309b890e1d2e76bf721edf576627
Lucy <lucynasztemberg@o2.pl>
2018-09-11 14:18:56 +0200

    Add getter and setter

--- src/main/java/pl/xcodesoftware/rekrutacja/Entities/CurrencyValue.java
+++ src/main/java/pl/xcodesoftware/rekrutacja/Entities/CurrencyValue.java
@@ -9,4 +9,12 @@ public class CurrencyValue {
 
     @JsonProperty("value")
     public double value;
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
